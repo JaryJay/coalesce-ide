496235686669ec44725ea5c0acfcab27f9d8af9d
JaryJay <47123100+JaryJay@users.noreply.github.com>
2022-05-31 17:27:32 -0400

    Frameless window

--- app/index.js
+++ app/index.js
@@ -1,6 +1,35 @@
-// app/index.js
-const path = require('path');
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const windowProperties = {
+    width: 800,
+    height: 600,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true,
+    },
+    frame: false
+};
+
+
+// ============================================================================
+//
+// I did not write this code!
+// Boilerplate code copied from
+// https://dev.to/olyno/how-to-create-an-electron-application-with-vite-im
+//
+// ============================================================================
+
+app.setUserTasks([
+    {
+        program: process.execPath,
+        arguments: '--new-window',
+        iconPath: process.execPath,
+        iconIndex: 0,
+        title: 'New Window',
+        description: 'Create a new window'
+    }
+]);
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -9,32 +38,27 @@ if (require('electron-squirrel-startup')) {
 
 const isDev = process.env.IS_DEV === 'true';
 
-function createWindow() {
+const createWindow = () => {
     // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true,
-        },
-    });
+    const mainWindow = new BrowserWindow(windowProperties);
+    mainWindow.setMenuBarVisibility(false);
 
     // Open the DevTools.
     if (isDev) {
         mainWindow.loadURL('http://localhost:3000');
-        mainWindow.webContents.openDevTools();
+        // mainWindow.webContents.openDevTools();
     } else {
         // mainWindow.removeMenu();
         mainWindow.loadFile(path.join(__dirname, 'build', 'index.html'));
     }
+    return mainWindow;
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createWindow();
+    const mainWindow = createWindow();
     app.on('activate', function () {
         // On macOS it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
